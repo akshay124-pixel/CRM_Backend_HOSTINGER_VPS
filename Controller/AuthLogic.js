31767030f2068456994075279a0e18193086e22b
Akshay Pratap Singh <akshaypratapsingh1244@gmail.com>
2025-12-20 17:36:24 +0530

    Done

--- Controller/AuthLogic.js
+++ Controller/AuthLogic.js
@@ -1,7 +1,7 @@
 const User = require("../Schema/Model");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../utils/config jwt");
-// Check CID
+
 const ChangePassword = async (req, res) => {
   try {
     const { currentPassword, newPassword, email } = req.body;
