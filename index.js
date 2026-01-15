ac3ffca500f3439dc63e6ffa1cf34dce04ba9d6f
Akshay Pratap Singh <akshay124-pixel>
2026-01-15 12:09:03 +0530

    Fixs New Version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + "/.env" });
 const express = require("express");
 const dbconnect = require("./utils/db.connect");
 const cors = require("cors");
